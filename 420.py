de36929a8c3ba9970a3602f38baebac0a6842eec
CheatCoder0 <45957441+CheatCoder0@users.noreply.github.com>
2020-05-03 00:29:27 -0500

    Delete 420.py

--- 420.py
+++
@@ -1,4 +0,0 @@
-from discord_webhook import DiscordWebhook
-
-webhook = DiscordWebhook(url="https://discordapp.com/api/webhooks/447960295022002190/M4ggwJ7-FeHyYmm_uZZw4AmAiOVyIjrDRPdtYGTfyIogQSq03JN77EK9igDj2kk75d15", content="!ayy it's 4:20")
-response = webhook.execute()
